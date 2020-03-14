9e609fd9a0959119460ebca10ec1c4a0669172fb
Stainley Lebron <saiyamans@gmail.com>
2020-03-14 18:42:12 -0400

    Integration

---
+++ root/product-services/src/test/java/com/salapp/microservices/product/services/ProductServicesIT.java
@@ -0,0 +1,16 @@
+package com.salapp.microservices.product.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Stainley Lebron
+ * @since 3/14/20.
+ */
+public class ProductServicesIT {
+
+    @Test
+    public void executeItegration() {
+        Assertions.assertEquals("5", "5");
+    }
+}
